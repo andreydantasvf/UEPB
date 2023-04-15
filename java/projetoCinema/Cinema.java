aa2a1152b6a6d91c26ff0e37928f91fac3ba92f7
Andrey Dantas <andreydantasvf@gmail.com>
2023-04-15 19:06:56 -0300

    Adicionando funcoes void

--- java/projetoCinema/Cinema.java
+++ java/projetoCinema/Cinema.java
@@ -9,28 +9,23 @@ public class Cinema {
 
         int opcao;
         ArrayList<Filme> filmes = new ArrayList();
-        int cadeiras[][] = new int[5][5];
         ArrayList<Integer> cadeirasOcupadas = new ArrayList();
 
         do {
+            menu("Bem vindo ao CINEMA!");
             System.out.println("[1] - Comprar ingresso");
             System.out.println("[2] - Listar filmes");
             System.out.println("[3] - Adicionar filme");
             System.out.println("[0] - Sair");
-            System.out.print("Escolha uma opcao: ");
+            System.out.print("Escolha uma opção: ");
             opcao = sc.nextInt();
             sc.nextLine();
 
             switch (opcao) {
                 case 1:
-                    int aux = cadeiras.length * cadeiras[0].length;
-                    for (int i = 0; i < cadeiras.length; i++) {
-                        for (int j = 0; j < cadeiras.length; j++) {
-                            System.out.printf("[%d] ", aux);
-                            aux--;
-                        }
-                        System.out.println();
-                    }
+                    menu("Sala do Cinema");
+                    listarCadeiras();
+
                     System.out.print("As seguintes cadeiras estao ocupadas: ");
                     for (int cadeira : cadeirasOcupadas) {
                         System.out.printf("%d ", cadeira);
@@ -46,12 +41,11 @@ public class Cinema {
                     }
                     break;
                 case 2:
-                    System.out.println("Lista dos filmes: ");
+                    menu("Lista dos filmes");
                     for(Filme filme : filmes) {
-                        System.out.println("Filme: ");
                         System.out.printf("Nome: %s%n", filme.nome);
                         System.out.printf("Data: %s%n", filme.data);
-                        System.out.printf("Horario: %s%n", filme.horario);
+                        System.out.printf("Horario: %s%n%n", filme.horario);
                     }
                     break;
                 case 3:
@@ -65,6 +59,11 @@ public class Cinema {
                     Filme novoFilme = new Filme(nome, data, horario);
                     filmes.add(novoFilme);
                     break;
+                case 0:
+                    System.out.println("Valeu chef!");
+                    break;
+                default:
+                    System.out.println("Opção inválida, tente novamente!");
             }
         } while (opcao != 0);
 
@@ -79,5 +78,24 @@ public class Cinema {
             }
         }
         return false;
+    }
+
+    public static void listarCadeiras() {
+        int cadeiras[][] = new int[5][5];
+
+        int aux = cadeiras.length * cadeiras[0].length;
+        for (int i = 0; i < cadeiras.length; i++) {
+            for (int j = 0; j < cadeiras.length; j++) {
+                System.out.printf("[%d] ", aux);
+                aux--;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void menu(String titulo) {
+        System.out.println("---------------------------");
+        System.out.printf("  %s%n", titulo);
+        System.out.println("---------------------------");
     }
 }
