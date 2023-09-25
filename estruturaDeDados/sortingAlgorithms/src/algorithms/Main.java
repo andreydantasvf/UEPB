533fbb3fc1924cb716c3b3fb94eb8b91ed3f7ba3
Andrey Dantas <andreydantasvf@gmail.com>
2023-09-25 15:13:19 -0300

    feat: add funcs to create arrays

--- estruturaDeDados/sortingAlgorithms/src/algorithms/Main.java
+++ estruturaDeDados/sortingAlgorithms/src/algorithms/Main.java
@@ -1,8 +1,12 @@
 package algorithms;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {64, 34, 22, 12, 22, 11, 90};
+        //int[] array = SortedAscendingArray(5000);
+        //int[] array = SortedDescendingArray(5000);
+        int[] array = UnsortedArray(10000);
 
         Ordenacao sort = new Ordenacao();
         System.out.println("Time bubbleSort: " + sort.bubbleSort(array.clone()));
@@ -13,5 +17,36 @@ public class Main {
         System.out.println("Time randomQuickSort: " + sort.random_quickSort(array.clone()));
         System.out.println("Time quickSortJava: " + sort.quickSort_Java(array.clone()));
         System.out.println("Time countingSort: " + sort.countingSort(array.clone()));
+    }
+
+    public static int[] SortedAscendingArray(int size) {
+        int[] ascendingArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            ascendingArray[i] = i;
+        }
+
+        return ascendingArray;
+    }
+
+    public static int[] SortedDescendingArray(int size) {
+        int[] descendingArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            descendingArray[i] = size - i - 1;
+        }
+
+        return descendingArray;
+    }
+
+    public static int[] UnsortedArray(int size) {
+        int[] unsortedArray = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            unsortedArray[i] = random.nextInt(size);
+        }
+
+        return unsortedArray;
     }
 }
